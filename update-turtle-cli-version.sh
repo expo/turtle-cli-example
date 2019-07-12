68af484756121f105fb8c4b9e085c22a1f43b3cc
Dominik Sokal <dominik.sokal@swmansion.com>
2019-07-12 13:04:18 +0200

    use envsubst instead of envsub

--- update-turtle-cli-version.sh
+++ update-turtle-cli-version.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 export TURTLE_VERSION_NEW=`npm show turtle-cli version`
-envsub --env TURTLE_VERSION_NEW .circleci/config.template.yml .circleci/config.yml
-envsub --env TURTLE_VERSION_NEW README.template.md README.md
-envsub --env TURTLE_VERSION_NEW .travis.template.yml .travis.yml
+envsubst '${TURTLE_VERSION_NEW}' < .circleci/config.template.yml > .circleci/config.yml
+envsubst '${TURTLE_VERSION_NEW}' < README.template.md > README.md
+envsubst '${TURTLE_VERSION_NEW}' < .travis.template.yml > .travis.yml
 
 git add .circleci/config.yml README.md .travis.yml
 git commit -m "update to turtle-cli@$TURTLE_VERSION_NEW"
