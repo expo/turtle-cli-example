d78b9fcd0a483c39e3b6795a04bda653ae684cec
Dominik Sokal <dominik.sokal@swmansion.com>
2020-09-23 11:13:30 +0200

    upgrade expo to 39

--- update-turtle-cli-version.sh
+++ update-turtle-cli-version.sh
@@ -3,7 +3,7 @@
 TAG=${1:-latest}
 
 export TURTLE_VERSION_NEW=`npm show turtle-cli@$TAG version`
-export APP_EXPO_SDK_VERSION="37.0.0"
+export APP_EXPO_SDK_VERSION="39.0.0"
 envsubst '${TURTLE_VERSION_NEW} ${APP_EXPO_SDK_VERSION}' < .circleci/config.template.yml > .circleci/config.yml
 envsubst '${TURTLE_VERSION_NEW} ${APP_EXPO_SDK_VERSION}' < README.template.md > README.md
 envsubst '${TURTLE_VERSION_NEW} ${APP_EXPO_SDK_VERSION}' < .travis.template.yml > .travis.yml
